b395d827d671ca0d26a2b7c2e076777e8149dac4
unknown <yakuel123@gmail.com>
2019-01-28 17:06:40 -0500

    yooo im gay for my friend on discord we were sending nudes

--- robotcam.py
+++ robotcam.py
@@ -1,7 +1,11 @@
 import numpy as np
 import cv2
 from networktables import NetworkTables
+import socket
+from client import Client
 
+client = Client()
+client.connect("127.0.0.1", "5802")
 cap = cv2.VideoCapture("http://10.11.55.11:5800/video")
 
 while(True):
@@ -13,8 +17,6 @@ while(True):
     ty = table.getNumber('ty0',None)
     ta = table.getNumber('ta0',None)
     ts = table.getNumber('ts0',None)
-
-    print(ta)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
