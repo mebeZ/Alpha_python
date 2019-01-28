b0ad88e68b544850b6b6362374114c4c92c3ae3e
unknown <yakuel123@gmail.com>
2019-01-28 17:40:38 -0500

    i LOVE dick

--- robotcam.py
+++ robotcam.py
@@ -5,8 +5,16 @@ import socket
 from client import Client
 
 client = Client()
-client.connect("127.0.0.1", "5802")
+client.connect("127.0.0.1",5802)
 cap = cv2.VideoCapture("http://10.11.55.11:5800/video")
+
+def twoLeftmost(tx0, tx1, tx2):
+    arr = []
+    arr.append(tx0)
+    arr.append(tx1)
+    arr.append(tx2)
+    arr.sort()
+    return arr[0], arr[1]
 
 while(True):
     # Capture frame-by-frame
@@ -16,7 +24,9 @@ while(True):
     tx = table.getNumber('tx0',None)
     ty = table.getNumber('ty0',None)
     ta = table.getNumber('ta0',None)
-    ts = table.getNumber('ts0',None)
+    tx0 = table.getNumber('tx0',None) 
+    tx1 = table.getNumber('tx1',None) 
+    tx2 = table.getNumber('tx2',None)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
