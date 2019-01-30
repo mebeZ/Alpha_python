cd8747048055bca9b74bb5978d55a61d22d67c82
unknown <yakuel123@gmail.com>
2019-01-30 15:55:36 -0500

    converted coordinates chud sucked my dick today

--- robotcam.py
+++ robotcam.py
@@ -5,6 +5,8 @@ import socket
 from client import Client
 
 cap = cv2.VideoCapture("http://10.11.55.11:5800/video")
+mid_x = 160
+mid_y = 120
 
 def ourindex3(tx0, tx1, tx2):
     orig = [tx0, tx1, tx2]
@@ -46,16 +48,24 @@ while(True):
             if determineleft(h) == True:
                 x = (table.getNumber("tx" + str(h[0]),None) + table.getNumber("tx" + str(h[1]),None)) / 2
                 y = (table.getNumber("ty" + str(h[0]),None) + table.getNumber("ty" + str(h[1]),None)) / 2
+
+                x_coord = float(mid_x * (x + 1))
+                y_coord = float(mid_y * (y + 1))
             if determineleft(h) == False:
                 x = (table.getNumber("tx" + str(h[2]),None) + table.getNumber("tx" + str(h[1]),None)) / 2
                 y = (table.getNumber("ty" + str(h[2]),None) + table.getNumber("ty" + str(h[1]),None)) / 2
-            print("X:" + str(x) +  " Y:" + str(y))
+                x_coord = float(mid_x * (x + 1))
+                y_coord = float(mid_y * (y + 1))
+
+            print("X:" + str(x_coord) +  " Y:" + str(y_coord))
         
         if tx2 == 0: 
             h = ourindex2(tx0,tx1)
             x = (table.getNumber("tx" + str(h[0]),None) + table.getNumber("tx" + str(h[1]),None)) / 2
             y = (table.getNumber("ty" + str(h[0]),None) + table.getNumber("ty" + str(h[1]),None)) / 2
-            print("X:" + str(x) +  " Y:" + str(y))
+            x_coord = float(mid_x * (x + 1))
+            y_coord = float(mid_y * (y + 1))
+            print("X:" + str(x_coord) +  " Y:" + str(y_coord))
 
 
     # Display the resulting frame
