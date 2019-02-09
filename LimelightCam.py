47c69032a58daf495a4063bd3a0bfa22d622c2d3
toyotaghost <tobiasalam@gmail.com>
2019-02-09 16:19:00 -0500

    styled code

--- LimelightCam.py
+++ LimelightCam.py
@@ -4,30 +4,39 @@ from networktables import NetworkTables
 
 class LimelightCam():
     def __init__(self): #connects to limelight server
-        self.cap = cv2.VideoCapture("http://10.11.55.11:5800/video")
+        self.cap   = cv2.VideoCapture("http://10.11.55.11:5800/video")
         self.mid_x = 160 # x screen resolution / 2
         self.mid_y = 120 # y screen resolution / 2
 
-    def determineleft(self, table, h): # Determines skew of contour
-        if table.getNumber("ts" + str(h),None) > -45:
-            return (True)
-        else:
-            return False
+    def isFacingLeft(self, table, h): # determines of contour is facing left
+        return table.getNumber("ts" + str(h),None) > -45:
 
-    def ourindex3(self, tx0, tx1, tx2): # sorting contours by x if 3 contours found
-        orig = [tx0, tx1, tx2]
-        arr = [tx0, tx1, tx2]
-        arr.sort()
-        indices = [orig.index(arr[0]), orig.index(arr[1]),orig.index(arr[2])]
-        return indices
-
-    def ourindex2(self, tx0, tx1): # sorting contours by x position if 2 contours found
-        orig = [tx0, tx1]
-        arr = [tx0, tx1]
-        arr.sort()
-        indices = [orig.index(arr[0]), orig.index(arr[1])]
-        return indices
-    
+    def addIndexs(self, arr):
+        for i, el in enumerate(arr):
+            newArr[i] = (i, el)
+    def sortWithIndex(self, txs): # sorting contours by tx
+        sortedTxs = txs.copy()
+        self.addIndexs(sortedTxs)
+        def compare(pair):
+            if self.isContour(self.getVal(pair)):
+                return self.getVal(pair)
+            return 1000 # pushes non-contours off to the side
+        sortedTxs.sort(key = compare)
+        return sortedTxs
+    def isContour(self, tx): # given tx determines if it is a contour
+        return tx != 0
+    def getVal(self, pair):
+        return pair[1]
+    def getIndex(self, pair):
+        return pair[0]
+    def center(table, i1, i2):
+        x = (table.getNumber("tx" + str(i1),None) + table.getNumber("tx" + str(i2),None)) / 2
+        y = (table.getNumber("ty" + str(i1),None) + table.getNumber("ty" + str(i2),None)) / 2
+        x_coord = float(self.mid_x * (1 + x))
+        y_coord = float(self.mid_y * (1 - y)) # converts offset into tangible coordinates
+        return x_coord, y_coord
+        
+        
     def getCrosshairs(self): 
         NetworkTables.initialize(server="10.11.55.2") # gets contours
         table = NetworkTables.getTable("limelight")
@@ -35,51 +44,32 @@ class LimelightCam():
         tx0 = table.getNumber('tx0',None) 
         tx1 = table.getNumber('tx1',None) 
         tx2 = table.getNumber('tx2',None)
-        
-        if ((tx0 != None) and (tx1 != None)):
-            
-            if tx2 != 0:
-                h = self.ourindex3(tx0,tx1,tx2) # Sorting
-                if (((self.determineleft(table, h[1])) and (self.determineleft(table, h[0]) == False)) or ((self.determineleft(table, h[1]) == False) and (self.determineleft(table, h[2])))):
-                    table.putBoolean("sawHatch",True)
-                else:
-                    table.putBoolean("sawHatch",False)
-                if self.determineleft(table, h[1]) == True:
-                    x = (table.getNumber("tx" + str(h[0]),None) + table.getNumber("tx" + str(h[1]),None)) / 2
-                    y = (table.getNumber("ty" + str(h[0]),None) + table.getNumber("ty" + str(h[1]),None)) / 2 # Averages x and y values together
-                    x_coord = float(self.mid_x * (x + 1))
-                    y_coord = 240 - float(self.mid_y * (y + 1)) # converts offset into tangible coordinates
-                if self.determineleft(table, h[1]) == False:
-                    x = (table.getNumber("tx" + str(h[2]),None) + table.getNumber("tx" + str(h[1]),None)) / 2
-                    y = (table.getNumber("ty" + str(h[2]),None) + table.getNumber("ty" + str(h[1]),None)) / 2
-                    x_coord = float(self.mid_x * (x + 1))
-                    y_coord = 240 - float(self.mid_y * (y + 1))
-                return x_coord, y_coord
+        sortedConts = self.sortWithIndex([tx0,tx1,tx2])
+        leftTx  = self.getVal(sortedConts[0])
+        midTx   = self.getVal(sortedConts[1])
+        rightTx = self.getVal(sortedConts[2])
+        leftI   = self.getIndex(sortedConts[0])
+        midI    = self.getIndex(sortedConts[1])
+        rightI  = self.getIndex(sortedConts[2])
 
-            elif tx2 == 0:
-                h = self.ourindex2(tx0,tx1)    
-                if (self.determineleft(table, h[1])):
-                    table.putBoolean("sawHatch",True)
-                else:
-                    table.putBoolean("sawHatch",False)                                                                                                                                                                                                                                          
-                x = (table.getNumber("tx" + str(h[0]),None) + table.getNumber("tx" + str(h[1]),None)) / 2
-                y = (table.getNumber("ty" + str(h[0]),None) + table.getNumber("ty" + str(h[1]),None)) / 2
-                x_coord = float(self.mid_x * (x + 1))
-                y_coord = 240 - float(self.mid_y * (y + 1))
-                return x_coord, y_coord
+        leftPair  = self.isContour(leftTx)  and self.isContour(midTx) and self.isFacingLeft(table,midI)
+        rightPair = self.isContour(rightTx) and self.isContour(midTx) and not(self.isFacingLeft(table,midI))
+
+        table.putBoolean("sawHatch",leftPair or rightPair)
+        
+        if leftPair:
+            return center(table,leftI,midI)
         else:
-            #cv2.imshow("frame", frame)
-            return 0,0
+            return center(table,rightI,midI)
+        
     
     def getFrames(self):
         NetworkTables.initialize(server="10.11.55.2") 
         table = NetworkTables.getTable("limelight")
-        table.putNumber("camMode",0)
         x_coord, y_coord = self.getCrosshairs()
         ret, frame = self.cap.read()
-        table.putNumber("camMode",1)
-        cv2.line(frame, (int(x_coord)+15, int(y_coord)), (int(x_coord)-15, int(y_coord)), (255, 0, 0), thickness=3, lineType=8)
-        cv2.line(frame, (int(x_coord), int(y_coord)+15), (int(x_coord), int(y_coord)-15), (255, 0, 0), thickness=3, lineType=8)
+        cv2.line(camera, (int(x_coord)+15, int(y_coord)), (int(x_coord)-15, int(y_coord)), (255, 0, 0), thickness=3, lineType=8)
+        cv2.line(camera, (int(x_coord), int(y_coord)+15), (int(x_coord), int(y_coord)-15), (255, 0, 0), thickness=3, lineType=8)
         ret, epic = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY),90])
         return epic.tobytes()
 
