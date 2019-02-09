0448045f7af566c56bfffb829aef078022cc231d
toyotaghost <tobiasalam@gmail.com>
2019-02-09 09:40:09 -0500

    new stuff

--- flask-server.py
+++ flask-server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "ok boss"
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def gen():
     camera = LimelightCam()
@@ -14,11 +15,6 @@ def gen():
         print(camera.checkForHatch())
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'll\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5802, debug=True)
